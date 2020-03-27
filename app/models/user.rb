c7538445d22eb492295daa89c3355062135481c3
Rory Hellier <rory.hellier@googlemail.com>
2020-03-27 16:46:59 +0000

    Added in full user model validations
     - Added name validation with presence, max length of 50 and disallowing of nil names
     - Added email validation with presence, max length of 255 and regex
     - Added password validation with presence, length min of 6 and disallowing of nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :name, :email, presence: true
-  validates :password, length: { min: 6 }
+  validates :name, presence: true, length: { maximum: 50 }, allow_nil: false
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: false
   has_secure_password
 end
