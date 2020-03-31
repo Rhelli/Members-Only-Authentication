023a28023151b2cd7c37a5dbd16fbd9a5a079f82
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-03-31 18:54:43 +0100

    Removed duplicated Log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    # @user.send_activation_email
-    flash[:success] = 'You signed up successfully'
-    redirect_to @user
+      log_in @user
+      flash[:success] = 'You signed up successfully'
+      redirect_to @user
     else
     render 'new'
     end
