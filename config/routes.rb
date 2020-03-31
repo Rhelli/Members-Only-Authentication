f5df73a745aa18055553a4fee02fa6fd1b1f38f9
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-03-31 17:35:56 +0100

    Add sign in form Add dropdown path for profile and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  root  'members_only#home'
-  get   '/signup', to: 'users#new'
-  post  '/signup', to: 'users#create'
+  
+  root   'members_only#home'
+  get    '/signup', to:  'users#new'
+  post   '/signup', to:  'users#create'
+  get    '/login',  to:  'sessions#new'
+  post   '/login',  to:  'session#create'
+  delete '/logout', to:  'session#destroy'
 
   resources :users
 end
