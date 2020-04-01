447ecb142d0e7f14cf785228557471128f45b172
Rory Hellier <rory.hellier@googlemail.com>
2020-04-01 11:57:21 +0100

    Added permenant cookies
     - Added new migration for remember_digest in user model
    
     - Added remember, authenticated and forget methods to the user model
    
     - Updated the sessions helper current_user and added forget method
     - Added new remember method in the sessions helper
     - Updated the sessions_controller destroy method to allow for signing out bugs
     - Added User.digest method to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_122000) do
+ActiveRecord::Schema.define(version: 2020_04_01_102314) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_122000) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_digest"
   end
 
 end
